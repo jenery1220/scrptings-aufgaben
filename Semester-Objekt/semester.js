03ec27afc7e009248f2afe893aa0ab3ebb77e506
Yan Feng <jenery1220@qq.com>
2012-11-14 12:28:44 +0100

    Daten und Strukturen

---
+++ Semester-Objekt/semester.js
@@ -0,0 +1,54 @@
+// =============================================================
+// = Aufgabe: Deine Semesterbelegung als strukturiertes Objekt =
+// =============================================================
+
+var mein_semester = {
+  student: {
+	name:"Yan Feng",
+	studiegang: "Interface Design",
+	studiesemester: 2,
+	welches: "WS",
+    jahr: "2012/2013",
+	},
+
+  veranstaltung:[
+   { 
+	titel: "Scriptings",
+    sws: 2,
+    credits:3,
+    workload:90
+    },
+   {
+	title:"Read & Write",
+	sws:2,
+	credits:3,
+    workload:90
+    },
+   {
+	title:"vvvv",
+	sws:2,
+	credits:2,
+    workload:90
+	},
+   {
+	title:"Proberaum",
+	sws:2,
+	credits:2,
+    workload:90
+	},
+   {
+	title:"?sthetik der Globalisierung",
+	sws:2,
+	credits:2,
+    workload:90
+	}
+   ]
+};
+
+JSON.stringify(mein_semester);
+//"{"student":{"name":"Yan Feng","studiegang":"Interface //Design","studiesemester":2,"welches":"WS","jahr":"2012/2013"},"veranstaltung":
+//[{"titel":"Scriptings","sws":2,"credits":3,"workload":90},
+//{"title":"Read & Write","sws":2,"credits":3,"workload":90},
+//{"title":"vvvv","sws":2,"credits":2,"workload":90},
+//{"title":"Proberaum","sws":2,"credits":2,"workload":90},
+//{"title":"?sthetik der Globalisierung","sws":2,"credits":2,"workload":90}]}"
